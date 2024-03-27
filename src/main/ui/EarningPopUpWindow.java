9e9647deba7f576c932f07e9fdb1acef12875845
Aadetri <atawara@students.cs.ubc.ca>
2024-03-27 00:47:48 -0700

    Implemented earning pop up window

--- src/main/ui/EarningPopUpWindow.java
+++ src/main/ui/EarningPopUpWindow.java
@@ -1,12 +1,72 @@
 package ui;
 
-import java.util.ArrayList;
+import model.Earning;
 
-public class EarningPopUpWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class EarningPopUpWindow extends JFrame implements ActionListener {
+
+    private GuiPersonalFinanceTracker parentClass;
+    private JButton submit;
+    private JTextField name = new JTextField();
+    private JTextField note = new JTextField();
+    private JTextField date = new JTextField();
+    private JTextField amount = new JTextField();
+
     public EarningPopUpWindow(GuiPersonalFinanceTracker guiPersonalFinanceTracker) {
+        this.parentClass = guiPersonalFinanceTracker;
+        this.setTitle("Add New Earning");
+        this.setResizable(false);
+        this.setSize(600, 250);
+        this.getContentPane().setBackground(new Color(18, 18, 18));
+        this.setLocationRelativeTo(null);
+
+        submit = new JButton("Submit Expense");
+        submit.addActionListener(this);
+
+        this.setLayout(new GridLayout(5, 2));
+
+        formatFrame();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 
-    public ArrayList<String> returnValues() {
-        return null;
+    private void formatFrame() {
+        JPanel container = new JPanel();
+        container.setBackground(new Color(18,18,18));
+
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("amount ($): ", "name: ", "note (optional): ",
+                "date (MM-DD-YYYY): "));
+        ArrayList<JTextField> inputs = new ArrayList<>(Arrays.asList(amount, name, note, date));
+
+        for (int i = 0; i < 4; i++) {
+            JLabel text = new JLabel();
+            text.setText(names.get(i));
+            text.setForeground(new Color(211, 211, 211, 211));
+
+            add(text);
+            add(inputs.get(i));
+        }
+        add(submit);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submit) {
+            double input1 = Double.parseDouble(amount.getText());
+            String input2 = name.getText();
+            String input3 = note.getText();
+            String input4 = date.getText();
+
+            Earning earning = new Earning(input1, input2, input3, input4);
+            parentClass.getAccount().addEarning(earning);
+            this.dispose();
+        }
     }
 }
